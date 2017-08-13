e5fbe4799db1aba76e01f7a4d59ec636fb500777
Andrew Whitley <awhitley1233@gmail.com>
2017-08-13 17:23:11 -0400

    add logic to normalize seed data

--- db/seeds.js
+++ db/seeds.js
@@ -11,7 +11,7 @@ Character.remove({})
       heroes.forEach((hero, index, arr) => {
         Character.create({
           name: hero.name,
-          description: hero.description,
+          description: hero.description.replace(/\s/g, '') ? hero.description : "No description available...",
           image_url: hero.thumbnail.path + '/portrait_uncanny.' + hero.thumbnail.extension
         }, (err, char) => {
           console.log(err || char.name)
